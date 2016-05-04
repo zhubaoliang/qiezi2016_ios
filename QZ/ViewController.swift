3bfddb4057c5e013fe8a5cb3ac6b1ca67aa0a272
zhubaoliang <2941242616@qq.com>
2016-05-04 18:12:45 +0800

    Initial Commit

---
+++ QZ/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  QZ
+//
+//  Created by ios on 16/5/4.
+//  Copyright © 2016年 北京校酷网络科技有限公司. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
